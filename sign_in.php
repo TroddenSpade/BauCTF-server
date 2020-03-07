3bd9fe67a347bed557e25250c35247e69507280f
TroddenSpade <parsasam1378@gmail.com>
2020-03-07 22:50:00 +0330

    Update sign_in.php

--- sign_in.php
+++ sign_in.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Access-Control-Allow-Origin: http://localhost:8080");
+header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
 header("Access-Control-Max-Age: 3600");
@@ -9,7 +9,32 @@ header("Access-Control-Allow-Headers: Origin,Content-Type, Access-Control-Allow-
 include_once 'config/config.php';
 global $mysqli;
 
-$ip = (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) ? $_SERVER["HTTP_CF_CONNECTING_IP"] : $_SERVER["REMOTE_ADDR"];
+
+if (isset($_SERVER['HTTP_CLIENT_IP'])){
+
+    $client_ip = $_SERVER['HTTP_CLIENT_IP'];
+
+} else if(isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+
+    $client_ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+
+} else if(isset($_SERVER['HTTP_X_FORWARDED'])) {
+
+    $client_ip = $_SERVER['HTTP_X_FORWARDED'];
+
+} else if(isset($_SERVER['HTTP_FORWARDED_FOR'])) {
+
+    $client_ip = $_SERVER['HTTP_FORWARDED_FOR'];
+
+} else if(isset($_SERVER['HTTP_FORWARDED'])) {
+
+    $client_ip = $_SERVER['HTTP_FORWARDED'];
+
+} else if(isset($_SERVER['REMOTE_ADDR'])) {
+
+    $client_ip = $_SERVER['REMOTE_ADDR'];
+
+}
 
 $rawBody = file_get_contents("php://input");
 $data = array();
@@ -18,7 +43,7 @@ $username = $mysqli->escape_string($data['username']);
 $password = $mysqli->escape_string($data['password']);
 $response = array();
 
-$result = $mysqli->query("SELECT token FROM users WHERE username='$username' AND password='$password' LIMIT 1");
+$result = $mysqli->query("SELECT token,open FROM users WHERE username='$username' AND password='$password' LIMIT 1");
 if ($result->num_rows > 0) {
 
     $user_row = $result->fetch_assoc();
@@ -27,10 +52,11 @@ if ($result->num_rows > 0) {
 
     $response['token'] = $token;
     $response['username'] = $username;
+    $response['open'] = $user_row['open'];
 
 } else {
     http_response_code(400);
-    $response['error'] = "User does not exist!";
+    $response['error'] = "invalid username or password!";
 }
 
 $response = json_encode($response);
