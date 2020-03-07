d66b5c60e7576ff1987c9dead6494b6d41d80739
TroddenSpade <parsasam1378@gmail.com>
2020-03-07 22:49:36 +0330

    Update submit.php

--- submit.php
+++ submit.php
@@ -9,8 +9,6 @@ header("Access-Control-Allow-Headers: Origin,Content-Type, Access-Control-Allow-
 include_once 'config/config.php';
 global $mysqli;
 
-$ip = (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) ? $_SERVER["HTTP_CF_CONNECTING_IP"] : $_SERVER["REMOTE_ADDR"];
-
 $rawBody = file_get_contents("php://input");
 $data = array();
 $data = json_decode($rawBody, true);
@@ -18,37 +16,50 @@ $data = json_decode($rawBody, true);
 $token = $mysqli->escape_string($data['token']);
 $ctf = $mysqli->escape_string($data['ctf']);
 $pid = $mysqli->escape_string($data['pid']);
+$bare_code = $mysqli->escape_string($data['bare_code']);
 
 $response = array();
 
 if (!empty($token) &&
-    !empty($ctf) &&
-    isset($pid)
-    ) {
-    $user_query_result = $mysqli->query("SELECT uid FROM users WHERE token = '$token' LIMIT 1");
-    if ($user_row = $user_query_result->fetch_assoc()) {
-        $uid = $user_row['uid'];
-        if ($mysqli->query("INSERT INTO submission(uid,pid,code) VALUES ('$uid','$pid','$ctf')")
-            === TRUE) {
-            $get_problem_result = $mysqli->query("SELECT pid FROM problems WHERE pid = '$pid' AND code = '$ctf' LIMIT 1");
-            if($problems = $get_problem_result->fetch_assoc()){
-                $response['msg'] = "Correct Code.";
-            }else{
-                $response['msg'] = "Wrong Code!";
-            }
-            http_response_code(200);
-        } else {
-            http_response_code(400);
-            $response['error'] = "Error Submitting: " . $mysqli->error;;
-        }
-    } else {
-        http_response_code(400);
-        $response['error'] = "User does not exists!";
-    }
+   !empty($ctf) &&
+   isset($pid)
+   ) {
+     http_response_code(400);
+     $response['error'] = "Time's Up :)";
+
+    //change open = 0 to open = 1 for accessing Open Users
+
+   $user_query_result = $mysqli->query("SELECT uid FROM users WHERE open = '1' and token = '$token' LIMIT 1");
+   if ($user_row = $user_query_result->fetch_assoc()) {
+       $uid = $user_row['uid'];
+       $get_problem_result = $mysqli->query("SELECT pid FROM problems WHERE pid = '$pid' AND code = '$ctf' LIMIT 1");
+       if($problems = $get_problem_result->fetch_assoc()){
+           if ($mysqli->query("INSERT INTO submission(uid,pid,code) VALUES ('$uid','$pid','$ctf')")
+           === TRUE) {
+               $response['msg'] = "Correct Flag.";
+           } else {
+               http_response_code(400);
+               $response['error'] = "Error Submitting: " . $mysqli->error;
+           }
+       }else{
+           if ($mysqli->query("INSERT INTO submission(uid,pid,code,bare_code) VALUES ('$uid','$pid','$ctf','$bare_code')")
+           === TRUE) {
+               $response['msg'] = "Wrong Flag!";
+           } else {
+               http_response_code(400);
+               $response['error'] = "Error Submitting: " . $mysqli->error;
+           }
+       }
+       http_response_code(200);
+
+   } else {
+       http_response_code(400);
+       $response['error'] = "you are not allowed to submit";
+   }
 
 } else {
-    http_response_code(400);
-    $response['error'] = "Insufficient data.";
+   http_response_code(400);
+   $response['error'] = "Insufficient data.";
 }
 $response = json_encode($response);
 echo $response;
