e4d7ba865327ea575133863347cd269f91313d95
TroddenSpade <parsasam1378@gmail.com>
2020-03-07 22:48:59 +0330

    Update register.php

--- register.php
+++ register.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Access-Control-Allow-Origin: http://localhost:8080");
+header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
 header("Access-Control-Max-Age: 3600");
@@ -17,34 +17,40 @@ $data = json_decode($rawBody, true);
 
 $username = $mysqli->escape_string($data['username']);
 $password = $mysqli->escape_string($data['password']);
+$open = $mysqli->escape_string($data['open']);
+$email = $mysqli->escape_string($data['email']);
 
 $response = array();
+    
+http_response_code(400);
+$response['error']="The Registration is closed !";
 
 if (
-    !empty($username) &&
-    !empty($password)
+   !empty($username) &&
+   !empty($password)
 ) {
-    $result = $mysqli->query("SELECT username FROM users WHERE username='$username'");
-    if ($result->num_rows > 0) {
-        http_response_code(400);
-        $response['message']="user already exists!";
-    } else {
-        $token = uniqid($username);
+   $result = $mysqli->query("SELECT username FROM users WHERE username='$username' or email='$email'");
+   if ($result->num_rows > 0) {
+       http_response_code(400);
+       $response['error']="username already exists!";
+   } else {
+       $token = uniqid($username);
 
-        if ($mysqli->query("INSERT INTO users(username,password,token) VALUES('$username','$password','$token')")
-            === TRUE) {
-            http_response_code(200);
-            $response['token']=$token;
-            $response['username']=$username;
-        } else {
-            http_response_code(400);
-            $response['message'] = "Error creating User: " . $mysqli->error;;
-        }
-    }
+       if ($mysqli->query("INSERT INTO users(username,password,token,open,email) VALUES('$username','$password','$token','$open','$email')")
+           === TRUE) {
+           http_response_code(200);
+           $response['token']=$token;
+           $response['username']=$username;
+           $response['open']=$open;
+       } else {
+           http_response_code(400);
+           $response['error'] = "Error creating User: " . $mysqli->error;
+       }
+   }
 
 } else {
-    http_response_code(400);
-    $response['message'] = "Incomplete Data.";
+   http_response_code(400);
+   $response['error'] = "Incomplete Data.";
 }
 
 $response = json_encode($response);
