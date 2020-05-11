48ccd2253666b646207f1ab21507f37dfee9de46
TroddenSpade <parsasam1378@gmail.com>
2020-05-12 01:25:25 +0430

    Create ConfirmPasswordController.php

---
+++ app/Http/Controllers/Api/ResetPasswordController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
+
+class ResetPasswordController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller is responsible for handling password reset requests
+    | and uses a simple trait to include this behavior. You're free to
+    | explore this trait and override any methods you wish to tweak.
+    |
+    */
+
+    use ResetsPasswords;
+
+    /**
+     * Where to redirect users after resetting their password.
+     *
+     * @var string
+     */
+//    protected $redirectTo = RouteServiceProvider::HOME;
+
+    protected function sendResetResponse(Request $request, $response)
+    {
+        return response([
+            'message' => trans($response),
+        ], 200);
+    }
+
+    protected function sendResetFailedResponse(Request $request, $response)
+    {
+        return response([
+            'message' => trans($response),
+        ], 422);
+    }
+}
