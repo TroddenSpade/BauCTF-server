709ff95ab2ac2156ec793a1b0c4c8278001024c3
TroddenSpade <parsasam1378@gmail.com>
2020-05-17 22:02:25 +0430

    Update EventController.php

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    //
+    public static function getLatestEvent(){
+        $event = \DB::table('events')
+            ->whereDate('end','>=',now())
+            ->orderBy('id','asc')
+            ->first();
+
+        if($event){
+            return $event;
+        }
+
+        return \DB::table('events')->first();
+    }
 }
