ceea30cb5ceee50cbd3ceec20afe6e87b525a374
TroddenSpade <parsasam1378@gmail.com>
2020-05-17 22:02:38 +0430

    Update ParticipantController.php

--- app/Http/Controllers/ParticipantController.php
+++ app/Http/Controllers/ParticipantController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Participant;
 use Illuminate\Http\Request;
 
-class ParticipantController extends Controller
-{
-    //
+class ParticipantController extends Controller{
+
+    public function store(Request $request){
+
+        $valid_data = $request->validate([
+            'e_id' => 'required',
+        ]);
+
+        $res = \DB::table('events')
+            ->where('id', $valid_data['e_id'])
+            ->whereDate('end', '>=', now())
+            ->exists();
+
+        if ($res) {
+            Participant::create(array_merge($valid_data, ['u_id' => auth()->user()->id]));
+            return Response(['message' => 'successfully done'], 200);
+        }
+
+        return Response(['message' => 'The event has finished'], 400);
+
+    }
 }
