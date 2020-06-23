1f99367c454cf9fce1751d42da63ee8b655969c2
troddenspade <parsasam1378@gmail.com>
2020-06-23 12:10:23 -0700

    entry code added

--- app/Http/Controllers/ParticipantController.php
+++ app/Http/Controllers/ParticipantController.php
@@ -11,16 +11,21 @@ class ParticipantController extends Controller{
 
         $valid_data = $request->validate([
             'e_id' => 'required',
+            'code' => 'required'
         ]);
 
         $res = \DB::table('events')
             ->where('id', $valid_data['e_id'])
             ->whereDate('end', '>=', now())
-            ->exists();
+            ->first();
+
 
         if ($res) {
-            Participant::create(array_merge($valid_data, ['u_id' => auth()->user()->id]));
-            return Response(['message' => 'successfully done'], 200);
+            if($res->open == 1 || \Hash::check($valid_data['code'],$res->code)) {
+                Participant::create(array_merge($valid_data, ['u_id' => auth()->user()->id]));
+                return Response(['message' => 'successfully done'], 200);
+            }
+            return Response(['message' => 'Wrong entry code'], 400);
         }
 
         return Response(['message' => 'The event has finished'], 400);
